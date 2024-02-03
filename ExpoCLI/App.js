b4b902c94f5ac4b871b0e7f258cbc8515a78eb37
Nitesh Kumar <imnksid@gmail.com>
2024-02-04 01:16:15 +0530

    Added navigation libraries, images & created Home screen

--- ExpoCLI/App.js
+++ ExpoCLI/App.js
@@ -1,22 +1,8 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import AppNavigation from "./src/navigation/app-navigator";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text className="text-red-500">
-        Open up App.js to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+  return <AppNavigation />;
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
