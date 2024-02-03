4ea12b0f36270d4a8d4896a11539f04c46a25aeb
Nitesh Kumar <imnksid@gmail.com>
2024-02-04 01:22:48 +0530

    Created an index file to export all the images

---
+++ ExpoCLI/src/assets/images/index.ts
@@ -0,0 +1,13 @@
+const IMAGES = {
+  avatar: require("./avatar.png"),
+  bg: require("./bg.png"),
+  cloud: require("./cloud.png"),
+  emptyStar: require("./emptyStar.png"),
+  fullStar: require("./fullStar.png"),
+  heavyrain: require("./heavyrain.png"),
+  line: require("./line.png"),
+  mist: require("./mist.png"),
+  moderaterain: require("./moderaterain.png"),
+  partlycloudy: require("./partlycloudy.png"),
+  sun: require("./sun.png"),
+};
