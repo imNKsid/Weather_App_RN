26521fb1e875245834b91c9e225f1e0fb5e8fa83
Nitesh Kumar <imnksid@gmail.com>
2024-02-04 22:03:20 +0530

    Added required packages & made UI complete

--- ExpoCLI/App.js
+++ ExpoCLI/App.js
@@ -1,5 +1,10 @@
 import React from "react";
 import AppNavigation from "./src/navigation/app-navigator";
+import { NativeWindStyleSheet } from "nativewind";
+
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
 
 const App = () => {
   return <AppNavigation />;
--- ExpoCLI/src/screens/home-screen.tsx
+++ ExpoCLI/src/screens/home-screen.tsx
@@ -1,10 +1,187 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  Image,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { theme } from "../theme";
+import { MagnifyingGlassIcon } from "react-native-heroicons/outline";
+import { CalendarDaysIcon, MapPinIcon } from "react-native-heroicons/solid";
+import { ICONS, IMAGES } from "../assets";
 
 const Home = () => {
+  const [showSearch, setShowSearch] = useState(false);
+  const [locations, setLocations] = useState([1, 2, 3]);
+
+  const handleLocation = (loc) => {
+    console.log("loc =>", loc);
+  };
+
   return (
-    <View>
-      <Text>Home</Text>
+    <View className="flex-1 relative">
+      <StatusBar style="light" />
+      <Image
+        blurRadius={70}
+        source={IMAGES.bg}
+        className="absolute h-full w-full"
+      />
+      <SafeAreaView className="flex flex-1">
+        {/* Search Section */}
+        <View style={{ height: "7%" }} className="mx-4 relative z-50">
+          <View
+            className="flex-row justify-end items-center rounded-full"
+            style={{
+              backgroundColor: showSearch ? theme.bgWhite(0.2) : "transparent",
+            }}
+          >
+            {showSearch ? (
+              <TextInput
+                placeholder="Search City"
+                placeholderTextColor={"lightgray"}
+                className="pl-6 h-10 pb-1 flex-1 text-base text-white"
+              />
+            ) : null}
+            <TouchableOpacity
+              onPress={() => setShowSearch(!showSearch)}
+              style={{ backgroundColor: theme.bgWhite(0.3) }}
+              className="rounded-full p-3 m-1"
+            >
+              <MagnifyingGlassIcon size={25} color={"white"} />
+            </TouchableOpacity>
+          </View>
+          {locations.length > 0 && showSearch ? (
+            <View className="absolute w-full bg-gray-300 top-16 rounded-3xl">
+              {locations.map((loc, index) => {
+                let showBorder = index + 1 != locations.length;
+                let borderClass = showBorder
+                  ? " border-b-2 border-b-gray-400"
+                  : "";
+                return (
+                  <TouchableOpacity
+                    key={index}
+                    className={
+                      "flex-row items-center border-0 p-3 px-4 mb-1" +
+                      borderClass
+                    }
+                  >
+                    <MapPinIcon size={20} color={"gray"} />
+                    <Text className="text-black text-lg ml-2">London, UK</Text>
+                  </TouchableOpacity>
+                );
+              })}
+            </View>
+          ) : null}
+        </View>
+
+        {/* Forecast Section */}
+        <View className="mx-4 flex justify-around flex-1 mb-2">
+          {/* Location */}
+          <Text className="text-white text-center text-2xl font-bold">
+            London
+            <Text className="text-lg font-semibold text-gray-300">UK</Text>
+          </Text>
+
+          {/* Weather image */}
+          <View className="flex-row justify-center">
+            <Image source={IMAGES.partlycloudy} className="w-52 h-52" />
+          </View>
+
+          {/* Degree Celcius */}
+          <View className="space-y-2">
+            <Text className="text-center font-bold text-white text-6xl ml-5">
+              23&#176;
+            </Text>
+            <Text className="text-center text-white text-xl tracking-widest">
+              Partly cloudy
+            </Text>
+          </View>
+
+          {/* Other Stats */}
+          <View className="flex-row justify-between mx-4">
+            <View className="flex-row space-x-2 items-center">
+              <Image source={ICONS.wind} className="h-6 w-6" />
+              <Text className="text-white font-semibold text-base">22km</Text>
+            </View>
+            <View className="flex-row space-x-2 items-center">
+              <Image source={ICONS.drop} className="h-6 w-6" />
+              <Text className="text-white font-semibold text-base">23%</Text>
+            </View>
+            <View className="flex-row space-x-2 items-center">
+              <Image source={ICONS.sun} className="h-6 w-6" />
+              <Text className="text-white font-semibold text-base">
+                6:05 AM
+              </Text>
+            </View>
+          </View>
+        </View>
+
+        {/* Forecast for next days */}
+        <View className="mb-2 space-y-3">
+          <View className="flex-row items-center mx-5 space-x-2">
+            <CalendarDaysIcon size={22} color={"white"} />
+            <Text className="text-white text-base">Daily Forecast</Text>
+          </View>
+          <ScrollView
+            horizontal
+            contentContainerStyle={{ paddingHorizontal: 15 }}
+            showsHorizontalScrollIndicator={false}
+          >
+            <View
+              className="flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4"
+              style={{ backgroundColor: theme.bgWhite(0.15) }}
+            >
+              <Image source={IMAGES.heavyrain} className="h-11 w-11" />
+              <Text className="text-white">Monday</Text>
+              <Text className="text-white text-xl font-semibold">13&#176;</Text>
+            </View>
+            <View
+              className="flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4"
+              style={{ backgroundColor: theme.bgWhite(0.15) }}
+            >
+              <Image source={IMAGES.heavyrain} className="h-11 w-11" />
+              <Text className="text-white">Tuesday</Text>
+              <Text className="text-white text-xl font-semibold">13&#176;</Text>
+            </View>
+            <View
+              className="flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4"
+              style={{ backgroundColor: theme.bgWhite(0.15) }}
+            >
+              <Image source={IMAGES.heavyrain} className="h-11 w-11" />
+              <Text className="text-white">Wednesday</Text>
+              <Text className="text-white text-xl font-semibold">13&#176;</Text>
+            </View>
+            <View
+              className="flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4"
+              style={{ backgroundColor: theme.bgWhite(0.15) }}
+            >
+              <Image source={IMAGES.heavyrain} className="h-11 w-11" />
+              <Text className="text-white">Thursday</Text>
+              <Text className="text-white text-xl font-semibold">13&#176;</Text>
+            </View>
+            <View
+              className="flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4"
+              style={{ backgroundColor: theme.bgWhite(0.15) }}
+            >
+              <Image source={IMAGES.heavyrain} className="h-11 w-11" />
+              <Text className="text-white">Friday</Text>
+              <Text className="text-white text-xl font-semibold">13&#176;</Text>
+            </View>
+            <View
+              className="flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4"
+              style={{ backgroundColor: theme.bgWhite(0.15) }}
+            >
+              <Image source={IMAGES.heavyrain} className="h-11 w-11" />
+              <Text className="text-white">Saturday</Text>
+              <Text className="text-white text-xl font-semibold">13&#176;</Text>
+            </View>
+          </ScrollView>
+        </View>
+      </SafeAreaView>
     </View>
   );
 };
